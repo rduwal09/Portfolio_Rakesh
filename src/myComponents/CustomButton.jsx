ea36ba9e34a6ce84bc6812147cdfc165d4ee85de
rduwal09 <rakeshduwal64@gmail.com>
2023-06-07 20:09:50 -0700

    my portfolio complete

---
+++ src/myComponents/CustomButton.jsx
@@ -0,0 +1,29 @@
+import './custom_button.css'
+import React from 'react'
+
+function CustomButton(props){
+    const onPressed = (event)=>{
+        console.log("button clicked on pressed : ", event)
+
+    }
+    return(
+        <button id={props.id} onClick={
+            props.onClick
+        } className={`btn ${props.colorClass}`}>{props.title}
+        {
+            props.showIcon ?
+            <span style={{marginLeft:"20px"}}></span>
+            :
+            <span></span>
+        }
+        {
+            props.showIcon && <span class="material-symbols-outlined">
+            download
+            </span>
+        
+        }
+        </button>
+    )
+}
+
+export default CustomButton
